d32ba8487c5fd9e477123d74c5cbec38bcea8c2e
Edward Tate <edward.tate@iohk.io>
2018-01-09 07:00:20 -0400

    Made wallet-state runnable / fixed naming issue.

--- run-wallet-state.sh
+++ run-wallet-state.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-stack exec cardano-launcher -- --system-start 0 --topology ./topology.yaml --configuration-file ./configuration.yaml --rebuild-db --keyfile secret.key
+stack exec cardano-wallet-state -- --system-start 0 --topology ./topology.yaml --configuration-file ./configuration.yaml --rebuild-db --keyfile secret.key
