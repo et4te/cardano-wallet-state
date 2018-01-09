3d79ba57aa0a329b922ce2a4d56f30db20533e0f
Edward Tate <edward.tate@iohk.io>
2018-01-09 08:01:00 -0400

    Cleanup Main.hs + comment what is going on.

--- app/Main.hs
+++ app/Main.hs
@@ -14,89 +14,51 @@ module Main where
 
 import           Universum hiding (bracket)
 
-import qualified Control.Foldl as L
-import           Control.Lens (makeLensesWith, lens)
 import           Control.Monad.Random.Strict (evalRandT)
-import           Control.Monad.Reader (withReaderT)
-import           Control.Monad.Morph (hoist)
-import           Data.Acid (AcidState, closeAcidState)
+import           Crypto.Random.Types (MonadRandom)
+import           Data.Acid (AcidState)
 import           Data.Default (def)
-import qualified Data.List.NonEmpty as NonEmpty
 import           Data.Maybe (fromJust)
 import qualified Data.Text as T
 import           Formatting (sformat, shown, (%))
-import           Mockable (Production, bracket, runProduction, currentTime)
+import           Mockable (Production, runProduction)
 import           Pos.AllSecrets (mkAllSecretsSimple)
-import           Pos.Block.Slog (HasSlogContext (..), HasSlogGState (..))
-import           Pos.Block.BListener (MonadBListener(..))
-import           Pos.Block.Logic (normalizeMempool)
-import           Pos.Block.Logic.VAR (applyBlocks, rollbackBlocks,
-                                      applyWithRollback, verifyAndApplyBlocks)
 import           Pos.Block.Types (Blund)
 import           Pos.Client.CLI (CommonNodeArgs (..), NodeArgs (..), SimpleNodeArgs (..))
 import qualified Pos.Client.CLI as CLI
-import           Pos.Client.Txp (MonadBalances (..), getOwnUtxo, getBalanceFromUtxo)
-import           Pos.Client.KeyStorage (getSecretKeysPlain, MonadKeysRead(..), getSecretDefault)
-import           Pos.Communication (ActionSpec (..), OutSpecs, WorkerSpec, worker)
-import           Pos.Core (Address, Coin, HasConfiguration, HeaderHash, headerHash,
-                           prevBlockL, GenesisData(..), genesisData, Timestamp(..),
-                           isRedeemAddress, HasPrimaryKey (..), BlockCount,
-                           gdBootStakeholders, genesisSecretKeys, makeAddress, AddrSpendingData (..),
-                           AddrStakeDistribution (..), AddrAttributes (..)
-                          )
-import           Pos.Core.Block (Block, MainBlockchain)
-import           Pos.Core.Block.Blockchain (GenericBlock (..))
-import           Pos.Core.Block.Main (Body (..))
-import           Pos.Core.Txp (Tx (..))
-import           Pos.Crypto (SecretKey(..), ShouldCheckPassphrase (..), PassPhrase (..),
+import           Pos.Communication (OutSpecs, WorkerSpec, worker)
+import           Pos.Core (Address, HasConfiguration, HeaderHash, headerHash, prevBlockL, GenesisData(..),
+                           genesisData, BlockCount, gdBootStakeholders, genesisSecretKeys, makeAddress,
+                           AddrSpendingData (..), AddrStakeDistribution (..), AddrAttributes (..))
+import           Pos.Core.Block (Block)
+import           Pos.Crypto (EncryptedSecretKey (..), SecretKey (..), ShouldCheckPassphrase (..), PassPhrase (..),
                              deriveHDPassphrase, deriveHDSecretKey, packHDAddressAttr,
                              encToPublic, encToSecret, firstHardened, mkEncSecretUnsafe)
-import           Pos.DB (getBlock, DBSum (..), MonadDB (..), MonadGState (..), NodeDBs, dbGetDefault, dbIterSourceDefault)
+import           Pos.Crypto.HD (HDAddressPayload (..))
+import           Pos.DB (getBlock)
 import           Pos.DB.DB (initNodeDBs)
 import           Pos.DB.Block (getUndo)
-import           Pos.DB.Block.Load (loadBlundsWhile, loadBlocksWhile)
-import           Pos.DB.BlockIndex (getTipHeader)
-import           Pos.DB.Class (MonadDBRead(..))
-import           Pos.Generator (BlockGenMode)
-import           Pos.Generator.Block (TxGenParams (..), BlockGenParams (..), genBlocks,
-                                      tgpTxCountRange)
-import           Pos.Generator.BlockEvent.DSL (BlockApplyResult (..), emitBlockApply, pathSequence)
-import           Pos.GState (getTip, getGStateImplicit, GStateContext, HasGStateContext(..))
-import           Pos.Infra.Configuration (HasInfraConfiguration)
-import           Pos.KnownPeers (MonadFormatPeers (..), MonadKnownPeers (..))
+import           Pos.Generator.Block (TxGenParams (..), BlockGenParams (..), genBlocks)
+import           Pos.GState (getTip)
 import           Pos.Launcher (HasConfigurations, NodeParams (..), LoggingParams(..), withConfigurations)
 import           Pos.Launcher (runRealBasedMode, runNode, loggerBracket, bracketNodeResources)
-import           Pos.Merkle (MerkleTree(..))
-import           Pos.Network.Types (HasNodeType (..), NodeType (..))
-import           Pos.Reporting (HasReportingContext (..))
-import           Pos.Shutdown (HasShutdownContext (..))
-import           Pos.Slotting (MonadSlots(..), MonadSlotsData(..), HasSlottingVar(..))
-import           Pos.Ssc.Types (HasSscContext (..), SscParams)
+import           Pos.Ssc.Types (SscParams)
 import           Pos.StateLock (Priority (..), withStateLock)
-import           Pos.Txp (MempoolExt, MonadTxpMem, MonadTxpLocal (..), txpGlobalSettings,
-                          txNormalize, txProcessTransaction, txProcessTransactionNoLock,
-                          GenesisUtxo(..), genesisUtxo, utxoToAddressCoinPairs,
-                          TxpGlobalSettings, TxPayload(..), TxOut(..), TxIn(..))
+import           Pos.Txp (txpGlobalSettings, TxpGlobalSettings)
 import           Pos.Update (updateTriggerWorker)
-import           Pos.Util.Chrono (OldestFirst (..), NewestFirst (..),
-                                  nonEmptyOldestFirst, nonEmptyNewestFirst)
-import           Pos.Util.CompileInfo (HasCompileInfo(..), retrieveCompileTimeInfo, withCompileInfo)
-import           Pos.Util.TimeWarp (CanJsonLog (..))
-import           Pos.Util.UserSecret (usVss, HasUserSecret(..))
-import           Pos.Util (HasLens (..), postfixLFields, mconcatPair, lensOf')
-import           Pos.WorkMode (MinWorkMode, EmptyMempoolExt, RealMode, RealModeContext(..))
-import           System.Wlog (HasLoggerName (..), LoggerName, logDebug, logInfo)
-import           System.Random (mkStdGen, randomIO)
-import           Pos.Util.OutboundQueue
-  (updatePeersBucketReader, formatKnownPeersReader)
+import           Pos.Util.CompileInfo (HasCompileInfo, retrieveCompileTimeInfo, withCompileInfo)
+import           Pos.Util.UserSecret (usVss)
+import           Pos.Util (mconcatPair, lensOf')
 
-import           Wallet.Mode (MonadWalletMode, WalletMode, WalletContext(..),
-                              realModeToWallet)
-import           Wallet.State (TransientState(..), PersistentState(..), ConstantEntry (..))
-import           Wallet.State (acquireTransientState)
-import           Wallet.State.Acid (openPersistentState, insertConstant, lookupConstant,
-                                    getDynamicSet)
+import           System.Wlog (LoggerName, logInfo)
+import           System.Random (mkStdGen)
 
+import           Wallet.Mode (MonadWalletMode, WalletContext(..), realModeToWallet)
+import           Wallet.State (TransientState(..), PersistentState(..))
+import           Wallet.State.Acid (openPersistentState, insertConstant, getDynamicSet)
+
+------------------------------------------------------------------------------
+-- Block Generation
 ------------------------------------------------------------------------------
 
 generateBlunds
@@ -123,6 +85,11 @@ generateBlunds secretKeys blockCount seed txCountRange maxOutputs =
     withCompileInfo def $ do
       evalRandT (genBlocks blockGenParams maybeToList) (mkStdGen seed)
 
+
+------------------------------------------------------------------------------
+-- Blockchain Traversal
+------------------------------------------------------------------------------
+
 prevBlock :: HasConfiguration => Block -> HeaderHash
 prevBlock = view prevBlockL
 
@@ -141,19 +108,26 @@ traverseChain tip i = do
       undo <- getUndo $ headerHash block
       traverseChain (prevBlock block) (i+1)
 
+
+------------------------------------------------------------------------------
+-- HD Address Derivation
+------------------------------------------------------------------------------
+
+secretKeyToAddress :: MonadRandom m => SecretKey -> PassPhrase -> m Address
 secretKeyToAddress (SecretKey xprv) passphrase = do
   encryptedSecretKey <- mkEncSecretUnsafe passphrase xprv
   pure $ encryptedSecretKeyToAddress encryptedSecretKey Nothing
 
+encryptedSecretKeyToAddress :: EncryptedSecretKey -> Maybe HDAddressPayload -> Address
 encryptedSecretKeyToAddress encryptedSecretKey hdAddressPayload =
   let publicKey = PubKeyASD $ encToPublic encryptedSecretKey in
   let addressAttributes = AddrAttributes hdAddressPayload BootstrapEraDistr in
     makeAddress publicKey addressAttributes
 
--- deriveTestHD :: SecretKey -> PassPhrase -> 
+deriveTestHD :: MonadRandom m => SecretKey -> PassPhrase -> m (SecretKey, Address)
 deriveTestHD rootKey passphrase = do
     let hdPassphrase = deriveHDPassphrase $ encToPublic rootKey
-    let hdAddressPayload = packHDAddressAttr hdPassphrase [a', b', c']
+    let _hdAddressPayload = packHDAddressAttr hdPassphrase [a', b', c']
     let (Just encryptedSecretKey) = deriveHDSecretKey checkPassphrase passphrase rootKey c'
     -- FIXME The address returned here is not correct since it contains no HD attribute
     -- but this is necessary for testing since block generation doesn't generate attributes.
@@ -170,12 +144,21 @@ deriveTestHD rootKey passphrase = do
 
     checkPassphrase = ShouldCheckPassphrase True
 
+------------------------------------------------------------------------------
+-- Workers hooked into the node at startup.
+------------------------------------------------------------------------------
+
+-- In reality this will be a http server which has access to the state as it is updated by
+-- the block listener callbacks. For testing I have simply added functions which generate
+-- blocks from genesis secrets and checks that the tip of the chain has been advanced. The
+-- actual state is expected to be updated in the onApplyBlocks callback of the BListener
+-- instance derivation.
 httpWorker
   :: (HasCompileInfo, MonadWalletMode m)
   => ([WorkerSpec m], OutSpecs)
 httpWorker = first pure $ worker mempty $ runHttpServer
   where
-    runHttpServer sendActions = do
+    runHttpServer _sendActions = do
       WalletContext{..} <- ask
 
       tip <- getTip
@@ -186,6 +169,7 @@ httpWorker = first pure $ worker mempty $ runHttpServer
       -- There must be at least two distinct secret keys.
       let (Just (secretKey1:secretKey2:_)) = genesisSecretKeys
 
+      -- The addresses can be used later to find relevant transactions for testing.
       address1 <- secretKeyToAddress secretKey1 (PassPhrase "")
       address2 <- secretKeyToAddress secretKey2 (PassPhrase "")
 
@@ -197,17 +181,13 @@ httpWorker = first pure $ worker mempty $ runHttpServer
       putText $ "[Main] Address 2 = " `T.append` show address2
       putText $ "[Main] HD Address (No Attributes) = " `T.append` show address3
 
-      putText "[Main] Looking up a constant piece of data in httpWorker ..."
-      userAddress <- lookupConstant wcPersistentState "<user_key>"
-      putText $ "[Main] User Address = " `T.append` show userAddress
-
-      putText "Lookup up a dynamic piece of data in httpWorker ..."
-
       -- Trigger onApplyBlocks by generating valid blocks
       putText "[Main] Generating valid blunds ..."
 
+      -- When generating blunds we must use a majority of secret keys which are 'genesis'.
       blunds <- generateBlunds [secretKey1,secretKey2,secretKey3] 1 1 (1,1) 1
 
+      -- Here simply show that the tip of the blockchain was updated by the blund generation.
       tip <- getTip
 
       putText $ "[Main] Latest block = " `T.append` show tip
@@ -216,26 +196,28 @@ httpWorker = first pure $ worker mempty $ runHttpServer
 
       return ()
 
+------------------------------------------------------------------------------
+-- Database functions called at startup of node (TEST).
+------------------------------------------------------------------------------
+
 initAcidState :: (AcidState PersistentState -> Production ()) -> Production ()
 initAcidState f =
   openPersistentState >>= f
 
-initTransientState :: (MonadIO m) => m (MVar TransientState)
-initTransientState =
-  newMVar $ TransientState 0
-
--- | For each dynamic piece of data within the set ...
 synchronise :: MonadIO m => AcidState PersistentState -> m ()
 synchronise acid = do
   putText "Synchronising dynamic set ..."
 
-  -- Fetch all dynamic pieces of data.
+  -- Fetch data which has a tip
   dynamicSet <- getDynamicSet acid
 
   putText $ show dynamicSet
 
-  -- Run
   return ()
+
+------------------------------------------------------------------------------
+-- Beyond this point, initialisation of node, logger configuration, CLI params.
+------------------------------------------------------------------------------
 
 actionWithWallet
     :: ( HasConfigurations
@@ -248,25 +230,22 @@ actionWithWallet sscParams nodeParams =
   bracketNodeResources nodeParams sscParams
     txpGlobalSettings
     initNodeDBs $ \nodeResources -> do
+        -- First the acid state database is initialised then a node is run.
         initAcidState $ \acid -> do
+            -- On initialisation we synchronise with the current state of the blockchain.
+            synchronise acid
+
+            --------------------------------------------------------------------------------
+            -- TESTING
+            --------------------------------------------------------------------------------
+
+            -- For testing purposes we create an address based on the first genesis secret
+            -- key and store it in the db as a constant piece of data. This will later allow
+            -- us to track blocks pertaining to this address.
+
             -- Create a wallet based on one of the genesis secret keys.
             address <- defaultAddress
             insertConstant acid "<user_key>" address
-
-            -- Generate blocks based on this secret.
-
-            -- Create a constant piece of data such as an address which is later used
-            -- to find relevant data and update the dynamic set.
-
-            synchronise acid
-
-            -- Create a dynamic piece of data (an address)
-            -- address <- defaultAddress
-
-            -- For each dynamic piece of data, we run the synchronisation process in
-            -- parallel across each.
-
-            -- synchronise psDynamicSet
 
             runRealBasedMode (walletMode acid) realModeToWallet nodeResources $
                 runNode nodeResources workers
@@ -275,22 +254,6 @@ actionWithWallet sscParams nodeParams =
     defaultAddress = do
       let (Just (secretKey1:_)) = genesisSecretKeys
       secretKeyToAddress secretKey1 (PassPhrase "")
-
---    initWallet = do
-
-      -- The main thread is responsible for initialising the persistent state. Later,
-      -- the http handlers can update the data. AcidState works fine here.
-
-      -- realistically store much more than an address but for demo purposes holds just
-      -- one address.
-
-      -- persistentState <- openPersistentState
-
-      -- The persistent state is used to filter the transient state upon initialisation.
-      -- blunds <- getTip >>= loadBlundsWhile (const True)
-      -- let blunds = map identity blunds
-
-      -- let initState = filterState blunds address 0
 
     walletMode acid walletAction = do
         realModeContext <- ask
@@ -315,7 +278,6 @@ action (SimpleNodeArgs (commonNodeArgs@CommonNodeArgs {..}) (nodeArgs@NodeArgs {
   let vssSK = fromJust $ npUserSecret currentParams ^. usVss
   let sscParams = CLI.gtSscParams commonNodeArgs vssSK (npBehaviorConfig currentParams)
   actionWithWallet sscParams currentParams
-
 
 loggerName :: LoggerName
 loggerName = "node"
